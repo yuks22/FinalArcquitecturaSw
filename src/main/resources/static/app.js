64a6791b343a2993812608ad12f79f7c44509136
Itzi Reyes <itzireyes11@gmail.com>
2017-11-27 10:38:02 -0600

    jsdocs y corrección de alertas

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,14 @@
+/**
+ * El autor de las funciones de setConnected(), connect(), 
+ * disconnect() y showGreeting() es Spring.io
+ * 
+ */
 var stompClient = null;
 var user = null;
 
+/**
+ * Función que establece el estado de conectado.
+ */
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
@@ -13,6 +21,13 @@ function setConnected(connected) {
     $("#greetings").html("");
 }
 
+/**
+ * Función que se encarga de abrir una conexión a "/gs-guide-websocket".
+ * Si la conexión es exitosa, el cliente se subscribe a la destino "/topic/greetings",
+ * en donde el server estará publicando los mensajes.
+ * Cuando se reciba el mensaje, se agregará un elemento de párrafo al DOM para 
+ * mostrar el mensaje.
+ */
 function connect() {
     var socket = new SockJS('/gs-guide-websocket');
     stompClient = Stomp.over(socket);
@@ -25,12 +40,10 @@ function connect() {
     });
 }
 
-function hideUsername(u)
-{
-    if(u==1)
-    document.getElementById("usernameForm").style.display="none";
-}
-
+/**
+ * Función que se encarga de esconder los botones de connect y disconnect.
+ * @param  {[int]}
+ */
 function hideButtons(u)
 {
     if(u==1)
@@ -38,6 +51,11 @@ function hideButtons(u)
     document.getElementById("disconnect").style.display="block";
 
 }
+
+/**
+ * Función que se encarga de mostrar los botones o formas.
+ * @param  {[int]}
+ */
 function showButtons(u)
 {
   if(u==1)
@@ -47,6 +65,10 @@ function showButtons(u)
 
 }
 
+/**
+ * Función que lanza una alerta en caso de que el usuario no haya introducido su nombre.
+ * @return {[type]}
+ */
 function required()  
 {  
     var empt = document.forms["form1"]["text1"].value;
@@ -58,6 +80,9 @@ function required()
     }
 }
 
+/**
+ * Función que desconecta al cliente STOMP.
+ */
 function disconnect() {
     if (stompClient !== null) {
         stompClient.disconnect();
@@ -66,17 +91,31 @@ function disconnect() {
     console.log("Disconnected");
 }
 
+/**
+ * Función que obtiene el nombre del usuario.
+ */
 function sendUser(){
       user = document.getElementById("user").value;
 }
 
+/**
+ * Función que lanza alertas cuando el mensaje, el usuario o ambos están vacíos,
+ * si no es así recupera el nombre ingresado por el usuario y utiliza el cliente STOMP
+ * para enviarlo a GreetingController.greeting()
+ * @return {[boolean]} 
+ */
 function sendName() {
     var empt = document.forms["form1"]["text1"].value;
     var empt2 = document.forms["form2"]["text2"].value;
 
+    if (empt == "" && empt2 == "")  
+    {  
+        alert("Please enter your username and message");  
+        return false;  
+    }
     if (empt == "")  
     {  
-        alert("Please enter your message");  
+        alert("Please enter your username");  
         return false;  
     }
     if (empt2 == "")  
@@ -88,11 +127,17 @@ function sendName() {
     }   
 }
 
+/**
+ * Función que agrega el mensaje en un elemento de párrafo
+ */
 function showGreeting(message) {
     $("#greetings").append("<tr><td>" + message + "</td></tr>");
 
 }
 
+/**
+ * Llama a las funciones de connect(), disconnect(), sendName() y sendUser
+ */
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
